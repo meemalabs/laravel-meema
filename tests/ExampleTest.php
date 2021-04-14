199af60dc10bbe32af238df070660d2da84ae88d
Glenn Michael Torregosa <gtorregosa@gmail.com>
2021-04-14 11:53:10 +0800

    delete models

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,16 +2,13 @@
 
 namespace Meema\LaravelMeema\Tests;
 
-use Meema\LaravelMeema\Facades\Meema;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function it_can_be_instantiated()
+    public function true_is_true()
     {
-        $accessKey = Meema::getAccessKey();
-
-        dump($accessKey);
+        $this->assertTrue(true);
     }
 }
