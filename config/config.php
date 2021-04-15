d2d07d57b8d8080d04e2a26d91f291928e1451ec
Glenn Michael Torregosa <gtorregosa@gmail.com>
2021-04-15 11:14:43 +0800

    Collection Response

--- config/config.php
+++ config/config.php
@@ -5,4 +5,9 @@ return [
      * The publishable API key provided by the meema API
      */
     'api_key' => env('MEEMA_API_KEY', ''),
+
+    /*
+     * If you want to convert all response to a collection
+     */
+    'to_collection' => env('MEEMA_API_TO_COLLECTION', true),
 ];
