02acd33af81a145a2ad04f7155a2e232b895015d
Glenn Michael Torregosa <gtorregosa@gmail.com>
2021-04-27 12:29:32 +0800

    fix: update config file

--- config/config.php
+++ config/config.php
@@ -7,6 +7,11 @@ return [
     'api_key' => env('MEEMA_API_KEY', ''),
 
     /*
+     * The publishable API key provided by the meema API
+     */
+    'api_secret_key' => env('MEEMA_API_SECRET', ''),
+
+    /*
      * If you prefer to not work with a collections, you may set this
      * value to false and there will be an array response.
      */
